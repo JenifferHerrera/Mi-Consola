2bfc497859ebede28905f989ac070a8956ad27b1
JenifferHerrera <jmm.herrera@yavirac.edu.ec>
2021-06-18 18:40:44 -0500

    mi consola

---
+++ Persona.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleA
+{ class Persona
+    {
+        string nombre;
+        string apellido;
+        int edad;
+        int peso;
+        int estatura;
+        float nota;
+        bool EstadoCivil;
+        string Direccion;
+        byte MiembrosFamilia;
+        Fecha = Fecha
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Apellido { get { return apellido; } set { apellido = value; } }
+        public int Edad { get { return edad; } set { edad = value; } }
+        public int Peso { get { return peso; } set { peso = value; } }
+        public int Estatura { get { return estatura; } set { estatura = value; } }
+        public float Nota { get { return nota; } set { nota = value; } }
+        public bool EstadoCivil { get { return EstadoCivil; } set { EstadoCivil = value; } }
+        public float Direccion { get { return Direccion; } set { Direccion = value; } }
+        public byte MiembrosFamilia { get { return MiembrosFamilia; } set { MiembrosFamilia = value; } }
+        public float Fecha { get { return Fecha; } set { Fecha = value; } }
+
+        public string NombreCompleto
+        {
+            get { return $"{this.Nombre} {this.Apellido}"; } //interpolacion
+        }
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }        
+
+        public string Saludar()
+        {
+            return $"Hola... ¿como estas?, mi nombre es {this.Nombre}";
+        }
+       
+    }
+}
+
+            //ToDo:Jeniffer, debo leer 10 datos de una persona utilizando todos estos tipos 
+            //float
+            //double
+            //DateTime // System.DateTime
+            //bool
+
+
+       
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MiConsole
+{
+    class Program
+    {
+          static void Main(string[] datosPersona)
+        {
+            Console.WriteLine("Escriba su nombre: ");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su apellido: ");
+            var apellido = Console.ReadLine();
+
+            Console.WriteLine("Escriba su peso: ");
+            var peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba su estatura: ");
+            var estatura = int.Parse(Console.ReadLine());
+       
+            Console.WriteLine("Escriba su edad: ");
+            var edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba su nota: ");
+            var nota = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿Su estado civil?:");
+            var EstadoCivil = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Escriba su direccion: ");
+            var direccion = Console.ReadLine();
+
+            Console.WriteLine("Miembros de su familia:");
+            var MiembrosFamilia = Convert.ToByte(Console.ReadLine());
+
+            DateTime fecha = new DateTime(2021, 06, 17);
+            var Fecha = Console.ReadLine();
+
+            var amigo = new Persona
+            {
+                Nombre = nombre,
+                Apellido = apellido,
+                Peso = peso,
+                Estatura = estatura,
+                Edad = edad,
+                Nota = nota,
+                EstadoCivil = EstadoCivil,
+                direccion = direccion,
+                MiembrosFamilia = MiembrosFamilia,
+                Fecha = Fecha 
+                
+            };
+
+            
+            Console.WriteLine($"Hola {amigo.Nombre}" );
+            Console.WriteLine($"Tu apellido es: {amigo.Apellido}");
+            Console.WriteLine($"Tu edad es: {amigo.Edad}");
+            Console.WriteLine($"Tu peso es: {amigo.Peso}");
+            Console.WriteLine($"Tu estatura es: {amigo.estatura}");
+            Console.WriteLine($"Tu nota es: {amigo.Nota}");
+            Console.WriteLine($"Tu estado civil es: {amigo.EstadoCivil}");
+            Console.WriteLine($"Tu direccion es: {amigo.Direccion}");
+            Console.WriteLine($"Miembros de su familia son: {amigo.MiembrosFamilia}");
+            Console.WriteLine($"La Fecha es: {amigo.Fecha}");
+
+            Persona vecina = new("Carla", "Gomez");
+            Console.WriteLine($"Mi vecina se llama {vecina.NombreCompleto}");
+
+            Console.WriteLine("Escribe cualquier letra para terminar");
+
+            Console.Read();
+
+        }
+    }
+}
